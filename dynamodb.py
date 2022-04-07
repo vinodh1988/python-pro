fa6f6e7cbc883b0cbb4c7702fa0f1b8234eccacf
vinodhleo <vinodhleo@outlook.com>
2022-04-07 11:51:32 +0530

    table-create

--- dynamodb.py
+++ dynamodb.py
@@ -7,6 +7,29 @@ boto3.Session(
          aws_secret_access_key=os.getenv('SECRET_KEY')
 )
 
+
 dynamodb=boto3.resource("dynamodb")
+dynamodb.create_table(
+    TableName='People',
+    KeySchema=[
+      
+        {
+            "AttributeName": "Sno",
+            'KeyType':"HASH"
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            "AttributeName":"Sno",
+            "AttributeType":"N"
+        }],
+        ProvisionedThroughput={
+            'ReadCapacityUnits' :1,
+            'WriteCapacityUnits': 1
+        }
+    
+)
+
+print("Table created")
 
 print(dynamodb)
